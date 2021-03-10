05d870dd0fd64b41fa42af8dfda81ed983c7cc64
wjiaman <51515772+wjiaman@users.noreply.github.com>
2021-03-10 13:18:45 +0800

    2021/03/10

--- Solution.cpp
+++ Solution.cpp
@@ -1569,5 +1569,19 @@ void Solution::reverseKGroupHelper(ListNode *head, ListNode *tail) {
 }
 
 /*
- *
+ * [121. 买卖股票的最佳时机] 暴力法
  * */
+int Solution::maxProfit(vector<int> &prices) {
+    int min=prices[0], result=0;
+    for(int i=1; i<prices.size(); i++){
+        if(prices[i]<min){
+            min = prices[i];
+        }else{
+            if(prices[i]-min > result){
+                result = prices[i] - min;
+            }
+        }
+    }
+
+    return result;
+}
--- Solution.h
+++ Solution.h
@@ -165,6 +165,7 @@ public:
     static int wiggleMaxLength2(vector<int>& nums);
     static int wiggleMaxLength3(vector<int>& nums);     // [376.摆动序列] 贪心算法
     static ListNode* reverseKGroup(ListNode* head, int k);  // [25.K 个一组翻转链表] 链表
+    static int maxProfit(vector<int>& prices);  // [121. 买卖股票的最佳时机] 暴力法
 private:
     static void quickSort(int* arr, int left, int right);           // 递增快排
     static void quickSort(vector<int>& arr, int left, int right);    // 递增快排
